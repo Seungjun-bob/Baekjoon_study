381b434854654978b7ee2f9631b881f9e31540f7
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-03-17 11:48:27 +0900

    18870 dictionary print(제거)

--- 정렬/18870.py
+++ 정렬/18870.py
@@ -3,10 +3,7 @@ import sys
 N = int(input())
 x = list(map(int, sys.stdin.readline().split()))
 x2 = sorted(list(set(x)))
-print(x)
-print(x2)
-
 dic = {x2[i]: i for i in range(len(x2))}
-print(dic)
+
 for i in x:
     print(dic[i], end=' ')
